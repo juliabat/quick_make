c37967e83ceb92e7abe115903de162a2efa78b5d
Julia Batkiewicz <juliabat@rubin.ifi.uio.no>
2013-12-06 04:20:22 +0100

    links when a file is compiled

--- quickmake.sh
+++ quickmake.sh
@@ -8,11 +8,14 @@ function link {
     echo "linking" 
     gcc -o "$1" *.o
 }
+
+has_compiled=1
+
 if [ "$#" -ne 1  ]; then 
     echo "Usage: $ $0 name-of-program" ; exit 
 fi
 
-if [ -z `ls *.c` ]; then 
+if [ -z `ls *.c 2> /dev/null` ]; then 
     echo "Current directory does not contain any c-files"; exit
 fi
 
@@ -22,9 +25,10 @@ for cfile in *.c; do
     if [ ! -f "$objfile" ] || older_than "$objfile" "$cfile" ; then 
         echo "compiling $cfile, creating new objectfile $objfile " 
         gcc -c -o $objfile $cfile || { echo "gcc error, exiting"; exit; }
+        has_compiled=0  
     fi    
 done
 
-if [ ! -f "$1" ]; then
+if [ ! -f "$1" ] || [ $has_compiled ]; then
     link "$1"
-fi    
+fi   
