3598564d8867ffc1c4bbd4de5fa9d787ed4e0f10
Julia Batkiewicz <juliabat@student.matnat.uio.no>
2013-12-12 00:41:49 +0100

    used -ot instead of is_older

--- quickmake.sh
+++ quickmake.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-function older_than {
-    [ $(stat -c %Y "$1") -lt $(stat -c %Y "$2") ]
+cc=gcc
+
+function compile {
+    objfile=$1
+    cfile=$2
+    echo "Compiling $cfile, creating new objectfile $objfile " 
+    $cc -c -o $objfile $cfile 
 }
 
 function link {
     echo "Linking" 
-    gcc -o "$1" *.o
+    $cc -o "$1" *.o
 }
 
 has_compiled=1
@@ -21,9 +26,8 @@ fi
 
 for cfile in *.c; do
     objfile=${cfile%.*}.o
-    if [ ! -f "$objfile" ] || older_than "$objfile" "$cfile" ; then 
-        echo "Compiling $cfile, creating new objectfile $objfile " 
-        gcc -c -o $objfile $cfile || { echo "gcc error, exiting"; exit; }
+    if [ ! -f "$objfile" ] || [ "$objfile" -ot "$cfile" ] ; then 
+        compile $objfile $cfile || { echo "gcc error, exiting"; exit; }
         has_compiled=0  
     fi    
 done
